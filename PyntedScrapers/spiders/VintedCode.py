2b9c50661d00ef6f3ea1ce998e6dc2813f213b46
toffaa <legrandlouis1997@gmail.com>
2020-03-23 22:20:08 +0100

    Working on language

--- PyntedScrapers/spiders/VintedCode.py
+++ PyntedScrapers/spiders/VintedCode.py
@@ -6,11 +6,14 @@ class VintedcodeSpider(scrapy.Spider):
     allowed_domains = ['vinted.fr/']
     base_url = 'https://www.vinted.fr/vetements?'
     first_code = 0
-    last_code = 3000
+    last_code = 100
     tag = 'catalog'
+    lang = 'es-fr'
 
 
     def start_requests(self):
+        formatted_lang = '{"locale":"%s"}' % self.lang
+        scrapy.http.Request('https://www.vinted.fr/session_locale', method='PUT', body=formatted_lang)
         for i in range(self.first_code, self.last_code):
             yield scrapy.http.Request(self.base_url + self.tag + '[]=%d' % i, meta={'index':i}, callback=self.parse)
 
